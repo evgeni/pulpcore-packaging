52acaaf475b391c00de2c6cc02c9dafcaa26d8b3
Evgeni Golov <evgeni@golov.de>
2019-12-13 10:33:49 +0100

    start a dockerfile

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM centos:7
+
+RUN yum install -y epel-release && yum install -y postgresql-devel gcc python3 python3-devel python3-pip git git-annex rpm-build rpmdevtools && yum clean all
+
+COPY _generate_deps.sh /app/_generate_deps.sh
+
+ENV LANG en_US.UTF-8
+ENV LC_ALL en_US.UTF-8
+
+CMD ["/bin/bash", "/app/_generate_deps.sh"]
--- _generate_deps.sh
+++ _generate_deps.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
-yum install -y postgresql-devel gcc python3 python3-devel python3-pip
-pip3 install pulpcore pulp-file pulp-container
-pip3 freeze |sed '/certifi/d' > /app/pulpcore-requirements.txt
+
+PULPCORE_PACKAGES="pulpcore pulp-file pulp-container"
+PULPCORE_REQUIREMENTS="/app/pulpcore-requirements.txt"
+FOREMAN_PACKAGING="/app/foreman-packaging"
+
+pip3 install $PULPCORE_PACKAGES
+pip3 freeze |sed '/certifi/d' > $PULPCORE_REQUIREMENTS
+
+pip3 install git+https://github.com/evgeni/pyp2rpm.git@foreman#egg=pyp2rpm
+pip3 install git+https://github.com/pixelb/crudini.git@0.9.3#egg=crudini
+
+if [ -d $FOREMAN_PACKAGING ]; then
+  pushd $FOREMAN_PACKAGING
+
+  while read line; do
+    pkg=${line%==*}
+    version=${line#*==}
+    REWRITE_ON_SAME_VERSION=false ./add_pypi_package.sh ${pkg} ${version} katello-nightly-pulpcore-el7 pulpcore ./pyp2rpm/pulpcore.spec
+  done < $PULPCORE_REQUIREMENTS
+
+  popd
+fi
